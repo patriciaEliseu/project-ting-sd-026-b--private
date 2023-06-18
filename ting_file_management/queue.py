1bbc4f425b9b6cf797aea1bf3dc20c785f038c88
Patricia Eliseu <patyeliseu4@gmail.com>
2023-06-18 16:37:49 -0300

    add requisito 02

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -8,14 +8,19 @@ class Queue(AbstractQueue):
     def __len__(self):
         return len(self.queue)
 
+    # add elemento a fila e modificar o seu tamanho
     def enqueue(self, value):
         self.queue.append(value)
+
+    # removerá um elemento que está a mais tempo na fila e
+    # modificar o seu tamanho
 
     def dequeue(self):
         if len(self.queue) == 0:
             raise IndexError("Fila vazia, não é possível remover elementos.")
         return self.queue.pop(0)
 
+    # lança a excecão qdo o indice for invalido
     def search(self, index):
         if index < 0 or index >= len(self.queue):
             raise IndexError("Índice Inválido ou Inexistente")
